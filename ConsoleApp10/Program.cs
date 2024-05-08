eb4b972badabedd82f179f75ff65cd2598f42757
User <User@MAXDUST>
2024-05-08 19:09:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,35 @@
+﻿
+interface InterfaceBiba<T>
+{
+    void MethodA(T obj);
+}
+
+interface InterfaceBoba<U>
+{
+    void MethodB(U obj);
+}
+
+class MyClass<T, U> : InterfaceBiba<T>, InterfaceBoba<U>
+{
+    public void MethodA(T obj)
+    {
+        Console.WriteLine( obj);
+    }
+
+    public void MethodB(U obj)
+    {
+        Console.WriteLine(obj);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass<int, string> myClass = new MyClass<int, string>();
+
+        myClass.MethodA(10);
+
+        myClass.MethodB("Биб и боб");
+    }
+}
